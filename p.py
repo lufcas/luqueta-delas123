294d6bdd8097697918e299489afff5db1c618fea
lufcas <schiavo.lucas@escola.pr.gov.br>
2024-10-15 08:38:28 -0300

    ,

--- p.py
+++ p.py
@@ -1,1 +1,10 @@
+escolha = input('oque deseja fazer [L]istar [A]pagar ou [I]nserir')
 
+lista = []
+
+if escolha == 'I' or escolha == 'i':
+    item = input('oque deseja inserir na sua lista: ')
+    lista.append(item)
+    
+if escolha == 'A' or escolha == 'a':
+    
