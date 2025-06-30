767edf86cc8a0a0b6936880071ee90742f62fd83
lufcas <schiavo.lucas@escola.pr.gov.br>
2025-06-30 07:57:40 -0300

    a

--- p.py
+++ p.py
@@ -1,10 +1,36 @@
-escolha = input('oque deseja fazer [L]istar [A]pagar ou [I]nserir')
+dinheiro = input('Quanto dinheiro voce tem agora para sacar: ')
+dinheirof = float(dinheiro)
 
-lista = []
+c200  = 200
+c100 = 100
+c50 = 50
+c20 = 20
+c10 =10
+c5 =5
+c1 = 1
 
-if escolha == 'I' or escolha == 'i':
-    item = input('oque deseja inserir na sua lista: ')
-    lista.append(item)
-    
-if escolha == 'A' or escolha == 'a':
-    
+def sacar(qtd, dinheiro_sacado=0)
+    dinheirof = float(qtd)
+    while len(dinheirof) >= c200:
+        dinheiro_sacado += c200
+        dinheirof -= 200
+
+    while len(dinheirof) >= c100:
+        dinheiro_sacado += c100
+        dinheirof -= 100
+
+    while len(dinheirof) >= c50:
+        dinheiro_sacado += c50
+        dinheirof -= 50
+
+    while len(dinheirof) >= c50:
+        dinheiro_sacado += c50
+        dinheirof -= 50
+
+    while len(dinheirof) >= c50:
+        dinheiro_sacado += c50
+        dinheirof -= 50
+
+    while len(dinheirof) >= c50:
+        dinheiro_sacado += c1
+        dinheirof -= 1
